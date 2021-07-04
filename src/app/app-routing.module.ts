05149f3f7f1cbc7d6e4b955a0a03b1af034d5c52
CabesaDario <darioojeda_1995@hotmail.com>
2021-07-04 18:27:27 +0200

    establecidas rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router"
+
+import { PorCapitalComponent } from "./pais/pages/por-capital/por-capital.component";
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: PorPaisComponent,
+        pathMatch: 'full',
+    },
+    {
+        path: 'region',
+        component: PorRegionComponent
+    },
+    {
+        path: 'capital',
+        component: PorCapitalComponent
+    },
+    {
+        path: 'pais/:id', //Después de los dos puntos se le da nombre a ese segmento del enrutamiento, en es
+        //caso id 
+        component: VerPaisComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot( routes )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {}
