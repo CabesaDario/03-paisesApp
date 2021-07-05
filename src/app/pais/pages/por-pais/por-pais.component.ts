d6d25b7ffbf1251c43af6fbada5026f3b0a4f9c9
CabesaDario <57437897+CabesaDario@users.noreply.github.com>
2021-07-05 14:19:44 +0200

    busqueda por pais
    se parsea la respuesta http de la api de paises con las interfaces correspondientes.

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
@@ -9,11 +12,29 @@ import { Component } from '@angular/core';
 export class PorPaisComponent{
 
   termino: string = '';
+  hayError: boolean = false;
+  hayPaises: boolean = false;
+  paises: Country[] = [];
   
-  constructor() { }
+  constructor( private paisService: PaisService) { }
 
 
   buscar() {
-    console.log(this.termino)
+    this.hayError = false; 
+    console.log(this.termino);
+
+    this.paisService.buscarPais( this.termino )
+        .subscribe( ( paises ) => {
+          if(paises.length !== 0){  
+            this.paises = paises;
+            this.hayPaises = true;
+          }else{
+            this.hayPaises = false;
+          }
+          console.log( paises );
+        }, (err) => {
+          this.hayError = true;
+          this.paises = [];
+        });
   }
 }
