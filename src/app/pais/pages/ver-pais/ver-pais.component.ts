8a0444b2e79dbb9475ff3130b68d8e54700ab3e7
CabesaDario <57437897+CabesaDario@users.noreply.github.com>
2021-07-06 13:50:18 +0200

    pantalla ver pais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from "rxjs/operators";
+import { pipe } from 'rxjs';
+
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -10,23 +14,34 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country;
+
   constructor( 
       private activatedRoute: ActivatedRoute,
       private paisService: PaisService){
-          this.activatedRoute.params
-              .subscribe(({id}) => {
-                console.log(id);
-
-                this.paisService.getPaisPorAlpha( id )
-                  .subscribe(
-                    pais => {
-                      console.log(pais);
-                    });
-              });
+          
   } 
 
   ngOnInit(): void {
 
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.paisService.getPaisPorAlpha(id) ),
+        tap( console.log )
+      )
+      .subscribe( pais => {
+        this.pais = pais;
+      });
+    // this.activatedRoute.params
+    //           .subscribe(({id}) => {
+    //             console.log(id);
+
+    //             this.paisService.getPaisPorAlpha( id )
+    //               .subscribe(
+    //                 pais => {
+    //                   console.log(pais);
+    //                 });
+    //           });
   }
 
 }
